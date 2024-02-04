ba4494097585c4506c71b885c9a467e58db1fd88
Eduard8888 <eduardgazaryan@gmail.com>
2024-02-04 10:11:50 +0300

    ТЗ

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,10 @@
 from calculator import Calculator
 
+
+# Задание
+# Разработчику 1 - реализовать функцию умножения
+# Разработчику 2 - реализовать функцию умножения
+
 calc = Calculator()
 calc.sum(5 ,10)
 calc.print_last_rez()
